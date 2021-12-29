71d268e9bf217fe2168ce6e6bcd619e141389f10
Kacper Celiński <celinskikacper@gmail.com>
2021-12-29 16:19:48 +0100

    add timestamp fields for restaurant

--- tastery/app/admin.py
+++ tastery/app/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from app.models import Restaurant
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    fields = ('name', 'address', 'rating')
+    list_display = ('name', 'address', 'rating')
+
+
+admin.site.register(Restaurant, AuthorAdmin)
--- tastery/app/models.py
+++ tastery/app/models.py
@@ -5,3 +5,5 @@ class Restaurant(models.Model):
     name = models.CharField(max_length=50)
     address = models.CharField(max_length=50)
     rating = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
