e84eaff1fec49b58540b08bc060ee7334e8b7b2d
Kacper Celiński <celinskikacper@gmail.com>
2021-12-29 16:02:46 +0100

    add new model with migrations

--- tastery/app/models.py
+++ tastery/app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Restaurant(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=50)
+    rating = models.IntegerField()
