e3a5220d669ad5270da1a2a77a4227ada4bbf202
Kacper Celiński <celinskikacper@gmail.com>
2021-12-29 16:49:47 +0100

    add restaurants to views

--- tastery/app/views.py
+++ tastery/app/views.py
@@ -1,15 +1,23 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
+from .models import Restaurant
+
 
 def index(request: HttpRequest) -> HttpResponse:
 
-    return render(request, 'index.html')
+    all_restaurants = Restaurant.objects.all()
+    return render(request, 'index.html', {
+        'restaurants': all_restaurants
+    })
 
 
 def restaurants(request: HttpRequest) -> HttpResponse:
 
-    return render(request, 'restaurants.html')
+    all_restaurants = Restaurant.objects.order_by()
+    return render(request, 'restaurants.html', {
+        'restaurants': all_restaurants
+    })
 
 
 def gallery(request: HttpRequest) -> HttpResponse:
